dbb8f48c1c9a3aec865a06778471c3a934e2286b
MuhammadRidho <ramadhanjenius1@gmail.com>
2022-03-30 12:52:26 +0700

    add & update praktikum pertemuan 5 dan 6

---
+++ Praktikum-pertemuan6/redux/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+// import { createStore, compose } from 'redux';
+import MainReducer from './reducers/MainReducer';
+import './index.css';
+import App from './App';
+import * as registerServiceWorker from "./registerServiceWorker";
+import reportWebVitals from './reportWebVitals';
+import CreateTodo from "./containers/CreateTodo";
+import Table from "./containers/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f =>
+  f)(createStore)(MainReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <CreateTodo />
+    <Table />
+  </Provider>
+  , document.getElementById('root'));
+registerServiceWorker.unregister();
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
